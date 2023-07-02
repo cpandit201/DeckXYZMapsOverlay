4497e66374aaf1ec5a6dd9b5d931297f69f69c55
Chirag Pandit <42322819+cpandit201@users.noreply.github.com>
2023-07-02 06:57:29 +0000

    Update 250 files

--- src/index.js
+++ src/index.js
@@ -4,52 +4,81 @@ import { LeafletLayer } from "deck.gl-leaflet";
 import { MapView } from "@deck.gl/core";
 import { GeoJsonLayer, ArcLayer } from "@deck.gl/layers";
 
+import { Map } from "@here/xyz-maps-display";
+import { MVTLayer, TileLayer, SpaceProvider } from "@here/xyz-maps-core";
+
 // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const AIR_PORTS =
   "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
 
+const coords = {
+  longitude: -122.227791,
+  latitude: 37.781058,
+  zoom: 17,
+};
+
 const map = L.map(document.getElementById("map"), {
-  center: [51.47, 0.45],
-  zoom: 4,
+  center: [coords.latitude, coords.longitude],
+  zoom: coords.zoom,
 });
+
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 const deckLayer = new LeafletLayer({
+  debug: true,
   views: [
     new MapView({
       repeat: true,
     }),
   ],
-  layers: [
-    new GeoJsonLayer({
-      id: "airports",
-      data: AIR_PORTS,
-      // Styles
-      filled: true,
-      pointRadiusMinPixels: 2,
-      pointRadiusScale: 2000,
-      getPointRadius: (f) => 11 - f.properties.scalerank,
-      getFillColor: [200, 0, 80, 180],
-    }),
-    new ArcLayer({
-      id: "arcs",
-      data: AIR_PORTS,
-      dataTransform: (d) =>
-        d.features.filter((f) => f.properties.scalerank < 4),
-      // Styles
-      getSourcePosition: (f) => [-0.4531566, 51.4709959], // London
-      getTargetPosition: (f) => f.geometry.coordinates,
-      getSourceColor: [0, 128, 200],
-      getTargetColor: [200, 0, 80],
-      getWidth: 1,
-    }),
-  ],
+  layers: [],
 });
 map.addLayer(deckLayer);
 
 const featureGroup = L.featureGroup();
 featureGroup.addLayer(L.marker([51.4709959, -0.4531566]));
 map.addLayer(featureGroup);
+
+const YOUR_ACCESS_TOKEN = "AGB705k1T0Oyizl4K04zMwA";
+
+// Step1 - Set underlying MVT tiles layer
+const MVT = new MVTLayer({
+  name: "background layer",
+  // the minimum zoom level the layer is displayed
+  min: 1,
+  // the maximum zoom level the layer is displayed
+  max: 20,
+  remote: {
+    url:
+      "https://xyz.api.here.com/tiles/osmbase/512/all/{z}/{x}/{y}.mvt?access_token=" +
+      YOUR_ACCESS_TOKEN,
+  },
+});
+
+// Step 1.1 - setup the Map Display
+const display = new Map(document.getElementById("xyzmap"), {
+  zoomlevel: coords.zoom,
+  center: {
+    longitude: coords.longitude,
+    latitude: coords.latitude,
+  },
+  // add layers to the map
+  layers: [MVT],
+});
+
+// Add dataset layer to display object
+display.addLayer(dataTileLayer);
+
+display.addLayer(myLayer);
+
+window.deckLayer = deckLayer;
+window.xyzMap = display;
+
+// Update lat long of XYZ map upon move
+map.on("moveend", function () {
+  console.log(map.getCenter());
+  // display.setCenter();
+});
