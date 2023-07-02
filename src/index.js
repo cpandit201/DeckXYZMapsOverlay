c36cfd10d4b343c79cd4681cbbd771b3742594d2
Chirag Pandit <42322819+cpandit201@users.noreply.github.com>
2023-07-02 06:07:08 +0000

    Update 250 files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import { MapboxOverlay as DeckOverlay } from "@deck.gl/mapbox";
+/* eslint-disable */
+import * as L from "leaflet";
+import { LeafletLayer } from "deck.gl-leaflet";
+import { MapView } from "@deck.gl/core";
 import { GeoJsonLayer, ArcLayer } from "@deck.gl/layers";
-import mapboxgl from "mapbox-gl";
 
 // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const AIR_PORTS =
   "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZGVhZG5pZ2h0NyIsImEiOiJjanJzeWY1dGkwMWQ4NDNsZmJjaGNsemZ3In0.WglAlgSd9imV5C778hJ0cA";
-const map = new mapboxgl.Map({
-  container: "map",
-  style:
-    "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json",
-  center: [0.45, 51.47],
+const map = L.map(document.getElementById("map"), {
+  center: [51.47, 0.45],
   zoom: 4,
-  bearing: 0,
-  pitch: 30
 });
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
-const deckOverlay = new DeckOverlay({
+const deckLayer = new LeafletLayer({
+  views: [
+    new MapView({
+      repeat: true,
+    }),
+  ],
   layers: [
     new GeoJsonLayer({
       id: "airports",
@@ -29,15 +33,6 @@ const deckOverlay = new DeckOverlay({
       pointRadiusScale: 2000,
       getPointRadius: (f) => 11 - f.properties.scalerank,
       getFillColor: [200, 0, 80, 180],
-      // Interactive props
-      pickable: true,
-      autoHighlight: true,
-      onClick: (info) =>
-        // eslint-disable-next-line
-        info.object &&
-        alert(
-          `${info.object.properties.name} (${info.object.properties.abbrev})`
-        )
     }),
     new ArcLayer({
       id: "arcs",
@@ -49,10 +44,12 @@ const deckOverlay = new DeckOverlay({
       getTargetPosition: (f) => f.geometry.coordinates,
       getSourceColor: [0, 128, 200],
       getTargetColor: [200, 0, 80],
-      getWidth: 1
-    })
-  ]
+      getWidth: 1,
+    }),
+  ],
 });
+map.addLayer(deckLayer);
 
-map.addControl(deckOverlay);
-map.addControl(new mapboxgl.NavigationControl());
+const featureGroup = L.featureGroup();
+featureGroup.addLayer(L.marker([51.4709959, -0.4531566]));
+map.addLayer(featureGroup);
