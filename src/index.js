0428a461f728306ac2ad693cf6a88cfb3f00b487
Chirag Pandit <cpandit201@gmail.com>
2023-07-07 16:21:58 +0530

    HeatMaps layer viz

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ const INITIAL_VIEW_STATE = {
 const deckgl = new Deck({
   layers:
     // layersWorldAirports,
-    // layerHeatMap,
-  layerScatterplot,
+    layerHeatMap,
+  // layerScatterplot,
   canvas: "deck-canvas",
   width: "100%",
   height: "100%",
